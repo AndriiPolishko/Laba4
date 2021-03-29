f25a70d07c027c8b2c562f66635c0bd0e7f5c191
Zepee <ramp8a799@gmail.com>
2021-03-29 11:42:36 +0300

    first commit

---
+++ cmake-build-debug/CMakeFiles/Laba4.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Laba4.dir/main.cpp.obj"
+  "Laba4.exe"
+  "Laba4.exe.manifest"
+  "Laba4.pdb"
+  "libLaba4.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Laba4.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
