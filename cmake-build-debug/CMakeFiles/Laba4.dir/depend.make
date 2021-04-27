d3710bfa1a59c45ce648358d9617b75ce3b89e77
Zepee <ramp8a799@gmail.com>
2021-04-27 23:03:34 +0300

    Finally decompression class complete First Release!!!

--- cmake-build-debug/CMakeFiles/Laba4.dir/depend.make
+++ cmake-build-debug/CMakeFiles/Laba4.dir/depend.make
@@ -4,6 +4,10 @@
 CMakeFiles/Laba4.dir/LZ78_C.cpp.obj: LZ78_C.cpp
 CMakeFiles/Laba4.dir/LZ78_C.cpp.obj: LZ78_C.h
 
+CMakeFiles/Laba4.dir/LZ78_DC.cpp.obj: LZ78_DC.cpp
+CMakeFiles/Laba4.dir/LZ78_DC.cpp.obj: LZ78_DC.h
+
 CMakeFiles/Laba4.dir/main.cpp.obj: LZ78_C.h
+CMakeFiles/Laba4.dir/main.cpp.obj: LZ78_DC.h
 CMakeFiles/Laba4.dir/main.cpp.obj: ../main.cpp
 
--- main.cpp
+++ main.cpp
@@ -1,7 +1,9 @@
 #include "C:\Users\ramp8\Laboratorni_Z_OP\Laba4\cmake-build-debug\LZ78_C.h"
-
+#include "C:\Users\ramp8\Laboratorni_Z_OP\Laba4\cmake-build-debug\LZ78_DC.h"
 int main() {
-    LZ78_C comp("input.txt", "output.lz78");
-    comp.init();
+    //LZ78_C comp("input.txt", "output.lz78");
+    LZ78_DC decomp("output.lz78","decomp.txt");
+    decomp.init();
+    //comp.init();
     return 0 ;
 }
